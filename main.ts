30d08a52641fb0220744a0d82e2451c00e434f84
Josh Medeski <joshmedeski@Joshs-MacBook-Pro.local>
2025-06-15 08:07:06 -0500

    ✨ Add dynamic widget

--- main.ts
+++ main.ts
@@ -1,89 +1,144 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	App,
+	Editor,
+	ItemView,
+	MarkdownView,
+	Modal,
+	Notice,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+	WorkspaceLeaf,
+} from "obsidian";
 
-// Remember to rename these classes and interfaces!
+export const VIEW_TYPE_DYNAMIC_WIDGET = "dynamic-widget-view";
 
-interface MyPluginSettings {
+interface DynamicWidgetSettings {
 	mySetting: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: DynamicWidgetSettings = {
+	mySetting: "default",
+};
+
+export class DynamicWidgetView extends ItemView {
+	constructor(leaf: WorkspaceLeaf) {
+		super(leaf);
+	}
+
+	getViewType(): string {
+		return VIEW_TYPE_DYNAMIC_WIDGET;
+	}
+
+	getDisplayText(): string {
+		return "Dynamic Widget";
+	}
+
+	async onOpen(): Promise<void> {
+		const container = this.containerEl.children[1];
+		container.empty();
+		container.addClass("dynamic-widget-container");
+
+		const content = container.createEl("div", {
+			cls: "dynamic-widget-content",
+		});
+
+		content.createEl("h4", {
+			text: "Dynamic Widget",
+			cls: "dynamic-widget-title",
+		});
+
+		const infoEl = content.createEl("div", { cls: "dynamic-widget-info" });
+		infoEl.createEl("p", { text: "This is your dynamic widget!" });
+
+		const currentTime = content.createEl("div", {
+			cls: "dynamic-widget-time",
+		});
+		currentTime.createEl("strong", { text: "Current Time: " });
+		currentTime.createEl("span", {
+			text: new Date().toLocaleTimeString(),
+			cls: "time-display",
+		});
+
+		const actionButton = content.createEl("button", {
+			text: "Refresh",
+			cls: "dynamic-widget-button",
+		});
+
+		actionButton.addEventListener("click", () => {
+			const timeSpan = currentTime.querySelector(".time-display");
+			if (timeSpan) {
+				timeSpan.textContent = new Date().toLocaleTimeString();
+			}
+		});
+	}
+
+	async onClose(): Promise<void> {
+		// Cleanup when view is closed
+	}
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class DynamicWidget extends Plugin {
+	settings: DynamicWidgetSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		// Register the dynamic widget view
+		this.registerView(
+			VIEW_TYPE_DYNAMIC_WIDGET,
+			(leaf) => new DynamicWidgetView(leaf),
+		);
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		// Auto-activate the widget in the right sidebar
+		await this.activateView();
 
-		// This adds a simple command that can be triggered anywhere
+		// Add command to toggle the dynamic widget
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
+			id: "open-dynamic-widget",
+			name: "Open Dynamic Widget",
 			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
+				this.activateView();
+			},
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
+	}
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+	async activateView() {
+		// Remove any existing instances
+		this.app.workspace.detachLeavesOfType(VIEW_TYPE_DYNAMIC_WIDGET);
+
+		// Create and activate the view in right sidebar
+		const leaf = this.app.workspace.getRightLeaf(false);
+		if (!leaf) return;
+
+		await leaf.setViewState({
+			type: VIEW_TYPE_DYNAMIC_WIDGET,
+			active: true,
 		});
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		// Ensure the view is visible
+		const leaves = this.app.workspace.getLeavesOfType(
+			VIEW_TYPE_DYNAMIC_WIDGET,
+		);
+		if (leaves.length > 0) {
+			this.app.workspace.revealLeaf(leaves[0]);
+		}
 	}
 
 	onunload() {
-
+		// Clean up the view when plugin is disabled
+		this.app.workspace.detachLeavesOfType(VIEW_TYPE_DYNAMIC_WIDGET);
 	}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData(),
+		);
 	}
 
 	async saveSettings() {
@@ -97,38 +152,40 @@ class SampleModal extends Modal {
 	}
 
 	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		const { contentEl } = this;
+		contentEl.setText("Woah!");
 	}
 
 	onClose() {
-		const {contentEl} = this;
+		const { contentEl } = this;
 		contentEl.empty();
 	}
 }
 
 class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+	plugin: DynamicWidget;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: DynamicWidget) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("Setting #1")
+			.setDesc("It's a secret")
+			.addText((text) =>
+				text
+					.setPlaceholder("Enter your secret")
+					.setValue(this.plugin.settings.mySetting)
+					.onChange(async (value) => {
+						this.plugin.settings.mySetting = value;
+						await this.plugin.saveSettings();
+					}),
+			);
 	}
 }
