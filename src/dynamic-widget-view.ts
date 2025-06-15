4cabdd237324802936039d77f5c19f3f633b0cde
Josh Medeski <joshmedeski@Joshs-MacBook-Pro.local>
2025-06-15 17:19:10 -0500

    ✨ Show files created and modified on day

--- src/dynamic-widget-view.ts
+++ src/dynamic-widget-view.ts
@@ -37,29 +37,6 @@ export class DynamicWidgetView extends ItemView {
 		return "activity";
 	}
 
-	private dynamicHeaderText(
-		area: string | undefined,
-		activeFile: TFile | null,
-	): string {
-		if (area) return area;
-		if (!activeFile) return "Dynamic Widget";
-
-		// Date formatting for date-named files
-		const basename = activeFile.basename;
-		if (basename && basename.match(/^\d{4}-\d{2}-\d{2}$/)) {
-			const [year, month, day] = basename.split("-").map(Number);
-			const date = new Date(year, month - 1, day); // month is 0-indexed
-			return date.toLocaleDateString("en-US", {
-				weekday: "short",
-				month: "short",
-				day: "numeric",
-				year: "numeric",
-			});
-		}
-
-		return activeFile.basename;
-	}
-
 	private makeUlLinkListWithTitle(
 		title: string,
 		list: TFile[] | undefined,
@@ -126,6 +103,52 @@ export class DynamicWidgetView extends ItemView {
 			}
 			return areaFrontmatter.replace(/\[\[|\]\]/g, "") === area;
 		});
+	}
+
+	private getFilesByDayCreated(date: Date) {
+		const files = this.app.vault
+			.getFiles()
+			.filter((file) => {
+				const fileDate = new Date(file.stat.ctime);
+				return (
+					file.extension === "md" &&
+					fileDate.getFullYear() === date.getFullYear() &&
+					fileDate.getMonth() === date.getMonth() &&
+					fileDate.getDate() === date.getDate()
+				);
+			})
+			.sort((a, b) => {
+				return (
+					new Date(b.stat.ctime).getTime() -
+					new Date(a.stat.ctime).getTime()
+				);
+			});
+
+		const newFiles = this.makeUlLinkListWithTitle("Created", files);
+		this.contentEl.appendChild(newFiles);
+	}
+
+	private getFilesByDayModified(date: Date) {
+		const files = this.app.vault
+			.getFiles()
+			.filter((file) => {
+				const fileDate = new Date(file.stat.mtime);
+				return (
+					file.extension === "md" &&
+					fileDate.getFullYear() === date.getFullYear() &&
+					fileDate.getMonth() === date.getMonth() &&
+					fileDate.getDate() === date.getDate()
+				);
+			})
+			.sort((a, b) => {
+				return (
+					new Date(b.stat.mtime).getTime() -
+					new Date(a.stat.mtime).getTime()
+				);
+			});
+
+		const newFiles = this.makeUlLinkListWithTitle("Modified", files);
+		this.contentEl.appendChild(newFiles);
 	}
 
 	async onOpen(): Promise<void> {
@@ -204,55 +227,82 @@ export class DynamicWidgetView extends ItemView {
 		);
 	}
 
+	private determineActiveFileType(
+		activeFile: TFile | null,
+	): "area" | "day" | "other" {
+		if (!activeFile) return "other";
+		const metadata = this.app.metadataCache.getFileCache(activeFile);
+		if (metadata?.frontmatter?.area) return "area";
+		if (activeFile.basename.match(/^\d{4}-\d{2}-\d{2}$/)) return "day";
+		return "other";
+	}
+
+	private renderAreaContent(activeFile: TFile): void {
+		const metadata = this.app.metadataCache.getFileCache(activeFile);
+		const areaFrontmatter = metadata?.frontmatter?.area;
+		const area = areaFrontmatter.replace(/\[\[|\]\]/g, "");
+		this.contentEl.createEl("h2", { text: area });
+		const areaFiles = this.getFilesByArea(area);
+		if (areaFiles) {
+			const folders = this.filesByFolders(areaFiles);
+			// todo: loop through records
+			folders.forEach((folder) => {
+				const areaSection = this.makeUlLinkListWithTitle(
+					folder.folder,
+					folder.files,
+				);
+				if (areaSection) this.contentEl.appendChild(areaSection);
+			});
+		}
+	}
+
+	private renderDateContent(): void {
+		const activeFile = this.app.workspace.getActiveFile();
+		if (!activeFile) return;
+
+		const basename = activeFile.basename;
+		if (!basename.match(/^\d{4}-\d{2}-\d{2}$/)) return;
+
+		const [year, month, day] = basename.split("-").map(Number);
+		const date = new Date(year, month - 1, day); // month is 0-indexed
+		this.contentEl.createEl("h2", {
+			text: date.toLocaleDateString("en-US", {
+				weekday: "short",
+				month: "short",
+				day: "numeric",
+				year: "numeric",
+			}),
+		});
+
+		this.getFilesByDayCreated(date);
+		this.getFilesByDayModified(date);
+	}
+
 	private updateContent(): void {
 		if (!this.contentEl) return;
-
-		// Clear existing content
 		this.contentEl.empty();
 
 		const activeFile = this.app.workspace.getActiveFile();
-
-		let area: string | undefined = undefined;
-		if (activeFile) {
-			const metadata = this.app.metadataCache.getFileCache(activeFile);
-			const areaFrontmatter = metadata?.frontmatter?.area;
-			if (areaFrontmatter) {
-				area = areaFrontmatter.replace(/\[\[|\]\]/g, "");
-			}
-		}
-
-		// Header
-		this.contentEl.createEl("h2", {
-			text: this.dynamicHeaderText(area, activeFile),
-		});
-
 		if (!activeFile) {
 			this.contentEl.createEl("p", {
 				text: "No file is currently active",
-				cls: "dynamic-widget-no-file",
 			});
 			return;
 		}
 
-		// const projectList = this.getActiveProjects();
-		// if (projectList) {
-		// 	this.contentEl.createEl("h3", { text: "Active Projects" });
-		// 	this.contentEl.appendChild(projectList);
-		// }
-
-		if (area) {
-			const areaFiles = this.getFilesByArea(area);
-			if (areaFiles) {
-				const folders = this.filesByFolders(areaFiles);
-				// todo: loop through records
-				folders.forEach((folder) => {
-					const areaSection = this.makeUlLinkListWithTitle(
-						folder.folder,
-						folder.files,
-					);
-					if (areaSection) this.contentEl.appendChild(areaSection);
+		const activeFileType = this.determineActiveFileType(activeFile);
+		switch (activeFileType) {
+			case "area":
+				this.renderAreaContent(activeFile);
+				break;
+			case "day":
+				this.renderDateContent();
+				break;
+			default:
+				this.contentEl.createEl("p", {
+					text: "Other",
 				});
-			}
+				break;
 		}
 	}
 
